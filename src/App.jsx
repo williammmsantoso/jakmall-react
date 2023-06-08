86e0563fd9fa0fe3b0d67a7855c5cf1403fd6c07
william_santoso <william_santoso@igrow.asia>
2023-06-08 19:46:54 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
